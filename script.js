ee102de6a1d7ed120f2e5fb309fd978895bc5329
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-05-13 19:31:19 +0200

    Selecionando Variáveis

---
+++ script.js
@@ -0,0 +1,13 @@
+const html = document.querySelector('html');
+const timer = document.querySelector('#timer');
+const image = document.querySelector('.app__image');
+const title = document.querySelector('.app__title');
+/* Variáveis btn */
+const startBtn = document.querySelector('.app__card-primary-button');
+const focusBtn = document.querySelector('.app__card-button--foco');
+const shortBtn = document.querySelector('.app__card-button--curto');
+const longBtn = document.querySelector('.app__card-button--longo');
+/* Variáveis tipo temporizador */
+const focusTimer = 1500;
+const shortBreakTimer = 300;
+const longBreakTimer = 900;
