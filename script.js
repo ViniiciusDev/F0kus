b75cc17489a3bdb13cf13880ffc550bd21e15bc1
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-05-14 18:41:24 +0200

    Click Events and Change img and text

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Variáveis Elementos */
 const html = document.querySelector('html');
 const timer = document.querySelector('#timer');
 const image = document.querySelector('.app__image');
@@ -11,3 +12,41 @@ const longBtn = document.querySelector('.app__card-button--longo');
 const focusTimer = 1500;
 const shortBreakTimer = 300;
 const longBreakTimer = 900;
+
+/* Eventos de Click */
+focusBtn.addEventListener('click', () => {
+    alterarContexto('foco');
+})
+shortBtn.addEventListener('click', () => {
+    alterarContexto('descanso-curto');
+})
+longBtn.addEventListener('click', () => {
+    alterarContexto('descanso-longo');
+})
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    image.setAttribute('src', `/imagens/${contexto}.png`);
+    switch (contexto) {
+        case "foco":
+            title.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+            case "descanso-curto":
+                title.innerHTML = `
+                Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>
+                `
+            break;
+            case "descanso-longo":
+                title.innerHTML = `
+                Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+                `
+            break;
+        default:
+            break;
+    }
+}
