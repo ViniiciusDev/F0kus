7079c33f519ff8595b6fb726d6ba28a5e2a428ac
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-05-17 20:47:28 +0200

    Add Timer e formatador Date

--- script.js
+++ script.js
@@ -1,76 +1,80 @@
 /* Variáveis Elementos */
-const html = document.querySelector('html');
-const timer = document.querySelector('#timer');
-const image = document.querySelector('.app__image');
-const title = document.querySelector('.app__title');
+const html = document.querySelector("html");
+const timer = document.querySelector("#timer");
+const image = document.querySelector(".app__image");
+const title = document.querySelector(".app__title");
 /* Variáveis btn */
-const startPauseBtn = document.querySelector('#start-pause');
-const focusBtn = document.querySelector('.app__card-button--foco');
-const shortBtn = document.querySelector('.app__card-button--curto');
-const longBtn = document.querySelector('.app__card-button--longo');
-const allBtn = document.querySelectorAll('.app__card-button');
+const startPauseBtn = document.querySelector("#start-pause");
+const focusBtn = document.querySelector(".app__card-button--foco");
+const shortBtn = document.querySelector(".app__card-button--curto");
+const longBtn = document.querySelector(".app__card-button--longo");
+const allBtn = document.querySelectorAll(".app__card-button");
+const playAndPauseBtn = document.querySelector("#start-pause span");
+const playAndPauseBtnIcon = document.querySelector(
+    ".app__card-primary-butto-icon"
+);
 /* Variáveis Musica */
-const musicInput = document.querySelector('#alternar-musica');
+const musicInput = document.querySelector("#alternar-musica");
 const focusMusic = new Audio("/sons/luna-rise-part-one.mp3");
 /* As 3 variáveis abaixo vão pegar o som que deve ser reproduzino no play no pause e no fim */
 const audioPlay = new Audio("/sons/play.wav");
 const audioPause = new Audio("/sons/pause.mp3");
 const audioEnd = new Audio("/sons/beep.mp3");
 focusMusic.loop = true;
-/* Variáveis tipo temporizador */
-const focusTimer = 1500;
-const shortBreakTimer = 300;
-const longBreakTimer = 900;
 /* Variáveis temporizados */
 /* Temos a variável intervaloId que tem como valor null, pois deve ser zerado. */
-let temporizadorEmSegundos = 5;
+let temporizadorEmSegundos = 1500;
 let intervaloId = null;
 
 /* Eventos de Click */
 musicInput.addEventListener("change", () => {
-    if(focusMusic.paused)   {
+    if (focusMusic.paused) {
         focusMusic.play();
     } else {
         focusMusic.pause();
     }
-})
-focusBtn.addEventListener('click', () => {
-    alterarContexto('foco');
-    focusBtn.classList.add('active');
-})
-shortBtn.addEventListener('click', () => {
-    alterarContexto('descanso-curto');
-    shortBtn.classList.add('active');
-})
-longBtn.addEventListener('click', () => {
-    alterarContexto('descanso-longo');
-    longBtn.classList.add('active');
-})
+});
+focusBtn.addEventListener("click", () => {
+    temporizadorEmSegundos = 1500;
+    alterarContexto("foco");
+    focusBtn.classList.add("active");
+});
+shortBtn.addEventListener("click", () => {
+    temporizadorEmSegundos = 300;
+    alterarContexto("descanso-curto");
+    shortBtn.classList.add("active");
+});
+longBtn.addEventListener("click", () => {
+    temporizadorEmSegundos = 900;
+    alterarContexto("descanso-longo");
+    longBtn.classList.add("active");
+});
 
 function alterarContexto(contexto) {
+    mostrarTemporizador();
     allBtn.forEach(function (contexto) {
-        contexto.classList.remove('active');
+        contexto.classList.remove("active");
     });
-    html.setAttribute('data-contexto', contexto);
-    image.setAttribute('src', `/imagens/${contexto}.png`);
+    html.setAttribute("data-contexto", contexto);
+    image.setAttribute("src", `/imagens/${contexto}.png`);
     switch (contexto) {
         case "foco":
             title.innerHTML = `
                 Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>
-            `
+            `;
             break;
-            case "descanso-curto":
-                title.innerHTML = `
+        case "descanso-curto":
+            title.innerHTML = `
                 Que tal dar uma respirada?<br>
                 <strong class="app__title-strong">Faça uma pausa curta!</strong>
-                `
+                `;
             break;
-            case "descanso-longo":
-                title.innerHTML = `
+        case "descanso-longo":
+            title.innerHTML = `
                 Hora de voltar à superfície.<br>
                 <strong class="app__title-strong">Faça uma pausa longa.</strong>
-                `
+                `;
             break;
         default:
             break;
@@ -79,30 +83,33 @@ function alterarContexto(contexto) {
 
 /* Na arrow function de contagem regressiva, temos uma verificação aonde evitamos que os numeros do contador sejam negativos, além disso quando o temporizador for igual a zero ele irá tocar o som de audioEnd. */
 const contagemRegressiva = () => {
-    if(temporizadorEmSegundos <= 0) {
+    if (temporizadorEmSegundos <= 0) {
         audioEnd.play();
         stop();
-        alert('Tempo Finalizado');
         return;
     }
     /* Para começar a decrementar o valor de temporizador usamos a variável -= 1, pega sempre o valor existente e subtrai 1 */
     temporizadorEmSegundos -= 1;
-    console.log(temporizadorEmSegundos);
-}
+    mostrarTemporizador();
+};
 /* Encontramos o Event Listener para escutar o click e executar a função startOrPause */
-startPauseBtn.addEventListener('click', startOrPause)
+startPauseBtn.addEventListener("click", startOrPause);
 
 /* Na função startOrPause() temos uma condicional que tem como parâmetro o valor de intervaloId, além do play a musica de Pause do contador. */
 function startOrPause() {
     if (intervaloId) {
         audioPause.play();
         stop();
+        playAndPauseBtn.textContent = "Começar";
+        playAndPauseBtnIcon.setAttribute("src", "/imagens/play_arrow.png");
         return;
     }
-/* Abaixo encontramos o audio do Play para quando começarmos a contagem. */
+    /* Abaixo encontramos o audio do Play para quando começarmos a contagem. */
     audioPlay.play();
-    /* No intervaloId passamos o método setInterval com 2 parâmetros sendo ele a contagemRegressiva e o valor em segundos. */
     intervaloId = setInterval(contagemRegressiva, 1000);
+    playAndPauseBtn.textContent = "pause";
+    playAndPauseBtnIcon.setAttribute("src", "/imagens/pause.png");
+    /* No intervaloId passamos o método setInterval com 2 parâmetros sendo ele a contagemRegressiva e o valor em segundos. */
 }
 
 /* Aqui encontramos a função clearInterval, que vai limpar o valor de intervaloId e será repassado o valor de null. */
@@ -110,3 +117,13 @@ function stop() {
     clearInterval(intervaloId);
     intervaloId = null;
 }
+
+function mostrarTemporizador() {
+    const time = new Date(temporizadorEmSegundos * 1000);
+    const timeFormatado = time.toLocaleTimeString("pt-br", {
+        minute: "2-digit",
+        second: "2-digit",
+    });
+    timer.innerHTML = `${timeFormatado}`;
+}
+mostrarTemporizador();
